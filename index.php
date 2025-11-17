01e775e99380f7f40fd2e76b5195e19ef2fae7b7
nereafh <nereafernandezh@gmail.com>
2025-11-17 18:25:56 +0100

    Cambio Index.php

--- index.php
+++ index.php
@@ -9,9 +9,7 @@ ini_set('display_errors', 1);
     echo Template::header('Biblioteca');
     echo Template::nav();
 
-
-    $seccion = Campo::val('seccion') ?? '';
-    echo Template::seccion($seccion);
+    echo Template::seccion(Campo::val('seccion'));
 
 
     echo Template::footer(); 
